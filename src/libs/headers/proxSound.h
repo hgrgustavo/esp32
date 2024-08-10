ccd0cb41376a8735b7940fe6f8ced55513454fba
hgrgustavo <gustavohgrdev@gmail.com>
2024-08-10 11:49:46 -0300

    changing stuff

---
+++ src/libs/headers/proxSound.h
@@ -0,0 +1,14 @@
+#ifndef __proxSound_h
+#define __proxSound_h
+
+#include <Buzzer.h>
+
+
+class proxSound {
+    Buzzer buzzer(4);
+
+    public: 
+        static void play(int note, int duration) {
+            Buzzer::sound
+        }
+}
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,28 +1,52 @@
 // https://wokwi.com/projects/405028246434409473
 
+
 #include <HCSR04.h>
 #include <ESP32Servo.h>
+#include <Buzzer.h> // https://github.com/gmarty2000-ARDUINO/arduino-BUZZER
+#include <ArduinoSound.h> // https://github.com/arduino-libraries/ArduinoSound
 
-Servo myservo;
+
+
+#define BUZZER_PIN 4
+
+Servo servo;
 HCSR04 hc(14, 27);
+Buzzer buzzer(4, 5);
+
+
 
 void setup() {
-  myservo.attach(2);
+  servo.attach(2);
 
   Serial.begin(115200);
 };
 
 void loop() {
   while (hc.dist(1) < 0.001) {
-    for (int pos = 0; pos <= 180; pos += 1) { 
-      myservo.write(pos);
-      delay(15);                  
-    }                   
+    buzzer.begin(100);
 
-    for (int pos = 180; pos >= 0; pos -= 1) { 
-      myservo.write(pos);
-      delay(15);                                     
+    for (int pos = 0; pos <= 180; pos += 1) {
+      servo.write(pos);
+      delay(15);
     }
-    hc.dist(1);
+
+    buzzer.sound(C, 100); // Nota C com duração de 100ms
+    buzzer.sound(E, 100);
+    buzzer.sound(G, 100);
+
+    // Pausa
+    buzzer.pause(500);
+
+    // Repetir a sequência
+
+    for (int pos = 180; pos >= 0; pos -= 1) {
+      servo.write(pos);
+      delay(15);
+    }
   };
+
+  hc.dist(1);
 };
+
+
