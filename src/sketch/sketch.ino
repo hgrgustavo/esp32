bb4b8919cb78d3ed9a199d65f2be8d3ab858f6a9
hgrgustavo <gustavohgrdev@gmail.com>
2024-09-28 16:18:37 -0300

    adding lib folder, triggering buzzer

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,41 +1,39 @@
+#include <Buzzer.h>
+
 const int trigPin = 31;
 const int echoPin = 33;
-const int ioPin = 52;
+const int ioPin = 35;
 
 long duration;
 int distance;
+Buzzer buzzer(ioPin);
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(ioPin, INPUT);
   Serial.begin(300);
+  buzzer.begin(300);
 }
 
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
 
   // triggering buzzer
-  switch (distance) {
-    case 10:
-        tone(ioPin, 35, 3.0);
-
-    case 25:
-        tone(ioPin, 40, 4.0);
-
-    case 30:
-        tone(ioPin, 50, 2.0);
+  if (distance < 41) {
+    buzzer.distortion(NOTE_C3, NOTE_C5);
+    buzzer.distortion(NOTE_C5, NOTE_C3);
+    buzzer.end(1000);   
   }
 
   Serial.println(distance);
+  
   delay(1000);
 }
